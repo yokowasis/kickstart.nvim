30ecafb30efc135c3775e0fdd5e4a76f54d41299
Your Name <you@example.com>
2024-03-15 08:16:38 +0800

    asd

--- init.lua
+++ init.lua
@@ -686,7 +686,7 @@ require('lazy').setup({
           -- Manually trigger a completion from nvim-cmp.
           --  Generally you don't need this, because nvim-cmp will display
           --  completions whenever it has completion options available.
-          ['<C-Space>'] = cmp.mapping.complete {},
+          ['<C-x>'] = cmp.mapping.complete {},
 
           -- Think of <c-l> as moving to the right of your snippet expansion.
           --  So if you have a snippet that's like:
