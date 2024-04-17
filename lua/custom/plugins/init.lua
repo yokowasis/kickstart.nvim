105c8027509111f1b7d83b071d4ec0811f8a182e
Wasis Sasoko <yokowasis@nitro>
2024-04-17 18:41:17 +0800

    save

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -10,6 +10,12 @@ vim.o.guifont = 'JetBrainsMono NFM:h14'
 -- independent clipboard
 vim.opt.clipboard = ''
 
+-- set autoindent expandtab tabstop=2 shiftwidth=2
+vim.o.autoindent = true
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+
 require('tokyonight').setup {
   styles = {
     comments = { italic = false },
