77041c1177241b9dcc3f7fff185223c01310f2a7
Wasis Sasoko <yokowasis@nitro>
2024-06-30 23:59:46 +0800

    save

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -215,6 +215,10 @@ function CompileAndRun()
     fixCommand = fixCommand .. ' && sed -i \'s/w:pStyle w:val="TableCaption"/w:pStyle w:val="figure"/g\' ' .. xmlpath
     fixCommand = fixCommand .. ' && sed -i \'s/w:pStyle w:val="ImageCaption"/w:pStyle w:val="figure"/g\' ' .. xmlpath
     fixCommand = fixCommand .. ' && sed -i \'s/w:pStyle w:val="CaptionedFigure"/w:pStyle w:val="figure"/g\' ' .. xmlpath
+    fixCommand = fixCommand
+      .. '&& sed -i "s|<w:p><w:pPr><w:pStyle w:val=\\"tableStyle\\" /></w:pPr><w:r><w:t xml:space=\\"preserve\\">noborder</w:t></w:r></w:p><w:tbl><w:tblPr><w:tblStyle w:val=\\"simpletable\\" />|<w:tbl><w:tblPr><w:tblStyle w:val=\\"noborder\\" />|" '
+      .. xmlpath
+
     deleteCommand = 'rm -rf ' .. filename_without_extension
 
     local refCommand = ''
