e36ddfba504ed970a93bdc8d3b264cf99c9270db
Your Name <you@example.com>
2024-08-26 08:59:53 +0800

    save

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -797,8 +797,11 @@ vim.keymap.set('n', '<leader>b6', ':e ~/git/cbtadmin/<cr>', { desc = '[B] 6. CBT
 vim.keymap.set('n', '<leader>b7', ':e ~/git/labs<cr>', { desc = '[B] 7. LABS ', noremap = true, silent = false })
 vim.keymap.set('n', '<leader>b8', ':e ~/git/wiki-bimasoft<cr>', { desc = '[B] 8. Wiki Bimasoft ', noremap = true, silent = false })
 vim.keymap.set('n', '<leader>b9', ':e ~/git/labs/kuliah/s2/beasiswa/paper/paper.md<cr>', { desc = '[B] 9. Paper Beasiswa ', noremap = true, silent = false })
-vim.keymap.set('n', '<leader>ba0', ':e ~/git/adminlte/<cr>', { desc = '[B] 10. MMS (AdminLTE)', noremap = true, silent = false })
-vim.keymap.set('n', '<leader>ba1', ':e d:/xampp/htdocs/adminlte/<cr>', { desc = '[B] 10. MMS (AdminLTE)', noremap = true, silent = false })
+vim.keymap.set('n', '<leader>ba0', ':e ~/git/adminlte/<cr>', { desc = 'MMS (AdminLTE)', noremap = true, silent = false })
+vim.keymap.set('n', '<leader>ba1', ':e d:/xampp/htdocs/adminlte/<cr>', { desc = 'MMS (AdminLTE d:\\xampp\\)', noremap = true, silent = false })
+vim.keymap.set('n', '<leader>ba2', ':e ~/git/client.bimasoft.web.id/<cr>', { desc = 'client area', noremap = true, silent = false })
+vim.keymap.set('n', '<leader>bai', ':EditInitVim<cr>', { desc = 'Init Vim', noremap = true, silent = false })
+vim.keymap.set('n', '<leader>bab', ':EditBaseVim<cr>', { desc = 'Base Vim', noremap = true, silent = false })
 
 -- change language
 vim.keymap.set('n', '<leader>lcj', ':set ft=javascript<cr>', { desc = 'Javascript', noremap = true, silent = false })
