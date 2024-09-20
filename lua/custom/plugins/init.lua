3b60ee5c15d8cf02b240f75ae49bf0ed03c597a0
Your Name <you@example.com>
2024-09-20 13:02:01 +0800

    save

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -303,6 +303,7 @@ vim.keymap.set('i', '<C-s>', '<Esc>:w<cr>a', { desc = '[^s] Save Current File', 
 
 -- "Tab Navigation
 vim.keymap.set('n', '<leader><up>', ':tabnew<CR>', { desc = 'New Tab', noremap = true, silent = true })
+vim.keymap.set('n', '<leader>tn', ':-1tabnew<CR>', { desc = 'New Tab To The Left', noremap = true, silent = true })
 vim.keymap.set('n', '<leader><right>', ':tabnext<CR>', { desc = 'Next Tab', noremap = true, silent = true })
 vim.keymap.set('n', '<leader><left>', ':tabprevious<CR>', { desc = 'Previous Tab', noremap = true, silent = true })
 vim.keymap.set('n', '<leader><down>', ':tabclose<CR>', { desc = 'Close Tab', noremap = true, silent = true })
