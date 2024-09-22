97501b48daa0da264d5bbe1d3910db232193edf0
Your Name <you@example.com>
2024-09-22 08:33:38 +0800

    aaaa

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -812,6 +812,7 @@ vim.keymap.set('n', '<leader>bab', ':EditBaseVim<cr>', { desc = 'Base Vim', nore
 vim.keymap.set('n', '<leader>lcj', ':set ft=javascript<cr>', { desc = 'Javascript', noremap = true, silent = false })
 vim.keymap.set('n', '<leader>lco', ':set ft=json<cr>', { desc = 'JSON', noremap = true, silent = false })
 vim.keymap.set('n', '<leader>lcp', ':set ft=php<cr>', { desc = 'PHP', noremap = true, silent = false })
+vim.keymap.set('n', '<leader>lch', ':set ft=html<cr>', { desc = 'HTML', noremap = true, silent = false })
 vim.keymap.set('n', '<leader>lcy', ':set ft=python<cr>', { desc = 'Python', noremap = true, silent = false })
 
 -- nvimtree setup
