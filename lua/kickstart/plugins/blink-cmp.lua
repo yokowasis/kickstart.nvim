3dffc6d5a69362015bde8a152cb453299bab12a1
Your Name <you@example.com>
2025-04-18 19:21:14 +0800

    .

--- lua/kickstart/plugins/blink-cmp.lua
+++ lua/kickstart/plugins/blink-cmp.lua
@@ -58,6 +58,8 @@ return {
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
         preset = 'default',
+        ["<cr>"] = {"accept", "fallback"},
+        ['<c-x>'] = { function(cmp) cmp.show({ providers = { 'lsp' } }) end },
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
