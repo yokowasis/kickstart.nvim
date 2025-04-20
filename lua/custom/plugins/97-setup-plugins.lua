996dd2fe3964b9ade3df82dab6d83d032157d6a4
Your Name <you@example.com>
2025-04-20 18:50:04 +0800

    save

--- lua/custom/plugins/97-setup-plugins.lua
+++ lua/custom/plugins/97-setup-plugins.lua
@@ -1,41 +1,39 @@
 local ls = require 'luasnip'
 ls.config.set_config {
-    store_selection_keys = '<tab>'
+  store_selection_keys = '<tab>',
 }
-ls.filetype_extend('svelte', {'typescript'})
+ls.filetype_extend('svelte', { 'typescript' })
 
+require('luasnip.loaders.from_vscode').lazy_load {
+  paths = { '~/git/friendly-snippets' },
+}
 
 require('mini.surround').setup {
-    mappings = {
-        add = 'ra', -- Add surrounding in Normal and Visual modes
-        delete = 'rd', -- Delete surrounding
-        find = 'rf', -- Find surrounding (to the right)
-        find_left = 'rF', -- Find surrounding (to the left)
-        highlight = 'rh', -- Highlight surrounding
-        replace = 'rr', -- Replace surrounding
-        update_n_lines = 'rn' -- Update `n_lines`
-    }
+  mappings = {
+    add = 'ra', -- Add surrounding in Normal and Visual modes
+    delete = 'rd', -- Delete surrounding
+    find = 'rf', -- Find surrounding (to the right)
+    find_left = 'rF', -- Find surrounding (to the left)
+    highlight = 'rh', -- Highlight surrounding
+    replace = 'rr', -- Replace surrounding
+    update_n_lines = 'rn', -- Update `n_lines`
+  },
 }
 
 -- close window with x if it's not a main window
 vim.api.nvim_create_autocmd('FileType', {
-    pattern = {'checkhealth', 'fugitive*', 'git', 'help', 'lspinfo', 'netrw', 'notify', 'qf', 'query'},
-    callback = function()
-        vim.keymap.set('n', 'x', vim.cmd.close, {
-            desc = 'Close the current buffer',
-            buffer = true
-        })
-    end
+  pattern = { 'checkhealth', 'fugitive*', 'git', 'help', 'lspinfo', 'netrw', 'notify', 'qf', 'query' },
+  callback = function()
+    vim.keymap.set('n', 'x', vim.cmd.close, {
+      desc = 'Close the current buffer',
+      buffer = true,
+    })
+  end,
 })
 
-require('luasnip.loaders.from_vscode').lazy_load {
-    paths = {'~/git/friendly-snippets'}
-}
-
 -- Folding settings
-vim.opt.foldmethod = "indent"
+vim.opt.foldmethod = 'indent'
 
-require('nvim-treesitter.install').compilers = {'clang', 'gcc', 'zig'}
+require('nvim-treesitter.install').compilers = { 'clang', 'gcc', 'zig' }
 
 return {}
-
