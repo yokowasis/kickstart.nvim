cd566a611f19b1204cfb60ee79f96c71c57bc40e
Your Name <you@example.com>
2025-08-16 20:49:45 +0800

    fix

--- lua/custom/plugins/00-init.lua
+++ lua/custom/plugins/00-init.lua
@@ -285,16 +285,6 @@ return {
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
     ft = { 'markdown', 'codecompanion' },
   },
-  -- {
-  --   "OXY2DEV/markview.nvim",
-  --   lazy = false,
-  --   opts = {
-  --     preview = {
-  --       filetypes = { "markdown", "codecompanion" },
-  --       ignore_buftypes = {},
-  --     },
-  --   },
-  -- },
   {
     'echasnovski/mini.diff',
     config = function()
