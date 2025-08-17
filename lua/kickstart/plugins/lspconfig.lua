4ec5ab4dca33833f425c1d0fbbe2e32fd9d2508a
Your Name <you@example.com>
2025-08-17 14:13:24 +0800

    save

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -238,6 +238,7 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Lua formatter
+        'tailwindcss', -- Tailwind CSS LSP
         'prettierd',
         'clang-format',
         'clangd',
