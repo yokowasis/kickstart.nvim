3b4f32d83cbf43a959fcd186ba761ba9e65c0585
Your Name <you@example.com>
2026-05-04 18:45:00 +0800

    format

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -5,133 +5,157 @@
 -- Primarily focused on configuring the debugger for Go, but can
 -- be extended to other languages as well. That's why it's called
 -- kickstart.nvim and not kitchen-sink.nvim ;)
-
 ---@module 'lazy'
 ---@type LazySpec
 return {
-  -- NOTE: Yes, you can install new plugins here!
-  'mfussenegger/nvim-dap',
-  -- NOTE: And you can specify dependencies as well
-  dependencies = {
-    -- Creates a beautiful debugger UI
-    'rcarriga/nvim-dap-ui',
-
-    -- Required dependency for nvim-dap-ui
-    'nvim-neotest/nvim-nio',
-
-    -- Installs the debug adapters for you
-    'mason-org/mason.nvim',
-    'jay-babu/mason-nvim-dap.nvim',
-
-    -- Add your own debuggers here
-    'leoluz/nvim-dap-go',
-
-    -- Virtual text support for nvim-dap
+    -- NOTE: Yes, you can install new plugins here!
+    'mfussenegger/nvim-dap',
+    -- NOTE: And you can specify dependencies as well
+    dependencies = { -- Creates a beautiful debugger UI
+    'rcarriga/nvim-dap-ui', -- Required dependency for nvim-dap-ui
+    'nvim-neotest/nvim-nio', -- Installs the debug adapters for you
+    'mason-org/mason.nvim', 'jay-babu/mason-nvim-dap.nvim', -- Add your own debuggers here
+    'leoluz/nvim-dap-go', -- Virtual text support for nvim-dap
     {
-      'theHamsta/nvim-dap-virtual-text',
-      opts = {
-        enabled = true,
-        enabled_commands = true,
-        highlight_changed_variables = true,
-        highlight_new_as_changed = false,
-        show_stop_reason = true,
-        commented = false,
-        only_first_definition = true,
-        all_references = false,
-        clear_on_continue = false,
-        display_callback = function(variable, buf, stackframe, node, options)
-          if options.virt_text_pos == 'inline' then
-            return ' = ' .. variable.value:gsub("%s+", " ")
-          else
-            return variable.name .. ' = ' .. variable.value:gsub("%s+", " ")
-          end
+        'theHamsta/nvim-dap-virtual-text',
+        opts = {
+            enabled = true,
+            enabled_commands = true,
+            highlight_changed_variables = true,
+            highlight_new_as_changed = false,
+            show_stop_reason = true,
+            commented = false,
+            only_first_definition = true,
+            all_references = false,
+            clear_on_continue = false,
+            display_callback = function(variable, buf, stackframe, node, options)
+                if options.virt_text_pos == 'inline' then
+                    return ' = ' .. variable.value:gsub("%s+", " ")
+                else
+                    return variable.name .. ' = ' .. variable.value:gsub("%s+", " ")
+                end
+            end,
+            virt_text_pos = vim.fn.has 'nvim-0.10' == 1 and 'inline' or 'eol',
+            all_frames = false,
+            virt_lines = false,
+            virt_text_win_col = nil
+        }
+    }},
+    keys = { -- Basic debugging keymaps, feel free to change to your liking!
+    {
+        '<F5>',
+        function()
+            require('dap').continue()
         end,
-        virt_text_pos = vim.fn.has 'nvim-0.10' == 1 and 'inline' or 'eol',
-        all_frames = false,
-        virt_lines = false,
-        virt_text_win_col = nil
-      },
-    },
-  },
-  keys = {
-    -- Basic debugging keymaps, feel free to change to your liking!
-    { '<F5>', function() require('dap').continue() end, desc = 'Debug: Start/Continue' },
-    { '<F1>', function() require('dap').step_into() end, desc = 'Debug: Step Into' },
-    { '<F2>', function() require('dap').step_over() end, desc = 'Debug: Step Over' },
-    { '<F3>', function() require('dap').step_out() end, desc = 'Debug: Step Out' },
-    { '<leader>b', function() require('dap').toggle_breakpoint() end, desc = 'Debug: Toggle Breakpoint' },
-    { '<leader>B', function() require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ') end, desc = 'Debug: Set Breakpoint' },
-    -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-    { '<F7>', function() require('dapui').toggle() end, desc = 'Debug: See last session result.' },
-  },
-  config = function()
-    local dap = require 'dap'
-    local dapui = require 'dapui'
+        desc = 'Debug: Start/Continue'
+    }, {
+        '<F1>',
+        function()
+            require('dap').step_into()
+        end,
+        desc = 'Debug: Step Into'
+    }, {
+        '<F2>',
+        function()
+            require('dap').step_over()
+        end,
+        desc = 'Debug: Step Over'
+    }, {
+        '<F3>',
+        function()
+            require('dap').step_out()
+        end,
+        desc = 'Debug: Step Out'
+    }, {
+        '<leader>b',
+        function()
+            require('dap').toggle_breakpoint()
+        end,
+        desc = 'Debug: Toggle Breakpoint'
+    }, {
+        '<leader>B',
+        function()
+            require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+        end,
+        desc = 'Debug: Set Breakpoint'
+    }, -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
+    {
+        '<F7>',
+        function()
+            require('dapui').toggle()
+        end,
+        desc = 'Debug: See last session result.'
+    }},
+    config = function()
+        local dap = require 'dap'
+        local dapui = require 'dapui'
 
-    require('mason-nvim-dap').setup {
-      -- Makes a best effort to setup the various debuggers with
-      -- reasonable debug configurations
-      automatic_installation = true,
+        require('mason-nvim-dap').setup {
+            -- Makes a best effort to setup the various debuggers with
+            -- reasonable debug configurations
+            automatic_installation = true,
 
-      -- You can provide additional configuration to the handlers,
-      -- see mason-nvim-dap README for more information
-      handlers = {},
+            -- You can provide additional configuration to the handlers,
+            -- see mason-nvim-dap README for more information
+            handlers = {},
 
-      -- You'll need to check that you have the required things installed
-      -- online, please don't ask me how to install them :)
-      ensure_installed = {
-        -- Update this to ensure that you have the debuggers for the langs you want
-        'delve',
-      },
-    }
+            -- You'll need to check that you have the required things installed
+            -- online, please don't ask me how to install them :)
+            ensure_installed = { -- Update this to ensure that you have the debuggers for the langs you want
+            'delve'}
+        }
 
-    -- Dap UI setup
-    -- For more information, see |:help nvim-dap-ui|
-    ---@diagnostic disable-next-line: missing-fields
-    dapui.setup {
-      -- Set icons to characters that are more likely to work in every terminal.
-      --    Feel free to remove or use ones that you like more! :)
-      --    Don't feel like these are good choices.
-      icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
-      ---@diagnostic disable-next-line: missing-fields
-      controls = {
-        icons = {
-          pause = '⏸',
-          play = '▶',
-          step_into = '⏎',
-          step_over = '⏭',
-          step_out = '⏮',
-          step_back = 'b',
-          run_last = '▶▶',
-          terminate = '⏹',
-          disconnect = '⏏',
-        },
-      },
-    }
+        -- Dap UI setup
+        -- For more information, see |:help nvim-dap-ui|
+        ---@diagnostic disable-next-line: missing-fields
+        dapui.setup {
+            -- Set icons to characters that are more likely to work in every terminal.
+            --    Feel free to remove or use ones that you like more! :)
+            --    Don't feel like these are good choices.
+            icons = {
+                expanded = '▾',
+                collapsed = '▸',
+                current_frame = '*'
+            },
+            ---@diagnostic disable-next-line: missing-fields
+            controls = {
+                icons = {
+                    pause = '⏸',
+                    play = '▶',
+                    step_into = '⏎',
+                    step_over = '⏭',
+                    step_out = '⏮',
+                    step_back = 'b',
+                    run_last = '▶▶',
+                    terminate = '⏹',
+                    disconnect = '⏏'
+                }
+            }
+        }
 
-    -- Change breakpoint icons
-    -- vim.api.nvim_set_hl(0, 'DapBreak', { fg = '#e51400' })
-    -- vim.api.nvim_set_hl(0, 'DapStop', { fg = '#ffcc00' })
-    -- local breakpoint_icons = vim.g.have_nerd_font
-    --     and { Breakpoint = '', BreakpointCondition = '', BreakpointRejected = '', LogPoint = '', Stopped = '' }
-    --   or { Breakpoint = '●', BreakpointCondition = '⊜', BreakpointRejected = '⊘', LogPoint = '◆', Stopped = '⭔' }
-    -- for type, icon in pairs(breakpoint_icons) do
-    --   local tp = 'Dap' .. type
-    --   local hl = (type == 'Stopped') and 'DapStop' or 'DapBreak'
-    --   vim.fn.sign_define(tp, { text = icon, texthl = hl, numhl = hl })
-    -- end
+        -- Change breakpoint icons
+        -- vim.api.nvim_set_hl(0, 'DapBreak', { fg = '#e51400' })
+        -- vim.api.nvim_set_hl(0, 'DapStop', { fg = '#ffcc00' })
+        -- local breakpoint_icons = vim.g.have_nerd_font
+        --     and { Breakpoint = '', BreakpointCondition = '', BreakpointRejected = '', LogPoint = '', Stopped = '' }
+        --   or { Breakpoint = '●', BreakpointCondition = '⊜', BreakpointRejected = '⊘', LogPoint = '◆', Stopped = '⭔' }
+        -- for type, icon in pairs(breakpoint_icons) do
+        --   local tp = 'Dap' .. type
+        --   local hl = (type == 'Stopped') and 'DapStop' or 'DapBreak'
+        --   vim.fn.sign_define(tp, { text = icon, texthl = hl, numhl = hl })
+        -- end
 
-    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
-    dap.listeners.before.event_exited['dapui_config'] = dapui.close
+        dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+        dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+        dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
-    -- Install golang specific config
-    require('dap-go').setup {
-      delve = {
-        -- On Windows delve must be run attached or it crashes.
-        -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
-        detached = vim.fn.has 'win32' == 0,
-      },
-    }
-  end,
+        -- Install golang specific config
+        require('dap-go').setup {
+            delve = {
+                -- On Windows delve must be run attached or it crashes.
+                -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
+                detached = vim.fn.has 'win32' == 0
+            }
+        }
+    end
 }
