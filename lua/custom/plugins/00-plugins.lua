f13677c8392d02e54a60b1e4306c05635049159a
Your Name <you@example.com>
2026-07-29 19:39:24 +0800

    copilot bim pack

--- lua/custom/plugins/00-plugins.lua
+++ lua/custom/plugins/00-plugins.lua
@@ -15,6 +15,7 @@ vim.pack.add {
   { src = 'https://github.com/rcarriga/nvim-notify' },
   { src = 'https://github.com/folke/noice.nvim' },
   { src = 'https://github.com/theHamsta/nvim-dap-virtual-text' },
+  { src = 'https://github.com/zbirenbaum/copilot.lua' },
 }
 
 -- =============================================================================
