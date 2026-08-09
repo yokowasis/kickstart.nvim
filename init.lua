bc635997fa91561edb6016bd26420f64fd4bb326
Your Name <you@example.com>
2026-08-09 15:25:39 +0800

    Quick Commit

--- init.lua
+++ init.lua
@@ -771,39 +771,6 @@ do
     stylua = {}, -- Used to format Lua code
     svelte = {},
 
-    -- lua_ls = {
-    --   on_init = function(client)
-    --     client.server_capabilities.documentFormattingProvider = false -- Disable formatting (formatting is done by stylua)
-    --
-    --     if client.workspace_folders then
-    --       local path = client.workspace_folders[1].name
-    --       if path ~= vim.fn.stdpath 'config' and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then return end
-    --     end
-    --
-    --     client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-    --       runtime = {
-    --         version = 'LuaJIT',
-    --         path = { 'lua/?.lua', 'lua/?/init.lua' },
-    --       },
-    --       workspace = {
-    --         checkThirdParty = false,
-    --         -- NOTE: this is a lot slower and will cause issues when working on your own configuration.
-    --         --  See https://github.com/neovim/nvim-lspconfig/issues/3189
-    --         library = vim.tbl_extend('force', vim.api.nvim_get_runtime_file('', true), {
-    --           '${3rd}/luv/library',
-    --           '${3rd}/busted/library',
-    --         }),
-    --       },
-    --     })
-    --   end,
-    --   ---@type lspconfig.settings.lua_ls
-    --   settings = {
-    --     Lua = {
-    --       format = { enable = false }, -- Disable formatting (formatting is done by stylua)
-    --     },
-    --   },
-    -- },
-
     --
     -- Some languages (like typescript) have entire language plugins that can be useful:
     --    https://github.com/pmizio/typescript-tools.nvim
