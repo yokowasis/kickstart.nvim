dd87c98c3c1b166d7ba48578511452130b03c7d3
Your Name <you@example.com>
2026-08-24 17:54:49 +0800

    Quick Commit

--- lsp_install.bat
+++ lsp_install.bat
@@ -1,11 +1,13 @@
 @echo off
 title LSP Installer — Neovim (Global)
 
-npm i -g bash-language-server
-winget install LuaLS.lua-language-server
+winget install LuaLS.lua-language-server -y
 
 rustup component add rust-analyzer
 uv tool install basedpyright
 uv tool install ruff
 uv tool install clang-format
 uv tool install git+https://github.com/johnnymorganz/stylua
+
+npm i -g bash-language-server
+
