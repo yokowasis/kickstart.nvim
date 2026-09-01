d392cba3bbb3e3c851aa47ddd3d23e3ae8092c65
Your Name <you@example.com>
2026-09-01 10:26:32 +0800

    Update Neovim Config

--- init.lua
+++ init.lua
@@ -890,7 +890,7 @@ do
   vim.pack.add { gh 'stevearc/conform.nvim' }
   require('conform').setup {
     notify_on_error = false,
-    format_on_save = function(bufnr)
+    format_after_save = function(bufnr)
       -- You can specify filetypes to autoformat on save here:
       local enabled_filetypes = {
         lua = true,
