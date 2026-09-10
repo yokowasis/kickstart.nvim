c3f888a4c9390615d4720000a54ff0d503aadd9a
Your Name <you@example.com>
2026-09-10 15:58:24 +0800

    Quick Commit

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -7,12 +7,12 @@ vim.pack.add {
   'https://github.com/MunifTanjim/nui.nvim',
 }
 
-vim.keymap.set('n', '\\', '<Cmd>Neotree reveal<CR>', { desc = 'NeoTree reveal', silent = true })
+-- vim.keymap.set('n', '\\', '<Cmd>Neotree reveal<CR>', { desc = 'NeoTree reveal', silent = true })
 
 require('neo-tree').setup {
   filesystem = {
     follow_current_file = {
-      enabled = true,          -- This finds and focuses the file in the active buffer every time a new file is opened
+      enabled = true, -- This finds and focuses the file in the active buffer every time a new file is opened
       leave_dirs_open = false, -- Closes folders when you leave them (set to true if you prefer keeping them open)
     },
     window = {
